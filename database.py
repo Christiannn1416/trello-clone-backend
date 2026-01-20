de886236a63532707a7c41c06a677e378c1d6d46
Christian Muñoz <lalodm69@gmail.com>
2026-01-20 00:54:35 -0600

    se agrega .env

--- database.py
+++ database.py
@@ -1,10 +1,22 @@
+import os
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABSE_URL = "postgresql://trelloclonedb_user:ckF6txsRI51SYENJWXu5HqhecTC4xoDq@dpg-d5nc43ur433s739m09k0-a.ohio-postgres.render.com/trelloclonedb?sslmode=require"
+# Carga las variables del archivo .env a las variables de entorno del sistema
+# 1. CARGAR primero
+load_dotenv() 
 
-engine = create_engine(SQLALCHEMY_DATABSE_URL)
+# 2. OBTENER después
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+# 3. VERIFICAR (esto te ayudará a debuguear)
+if SQLALCHEMY_DATABASE_URL is None:
+    raise ValueError("ERROR: La variable DATABASE_URL no se encontró en el archivo .env")
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit = False, autoflush = False, bind = engine)
 
 Base = declarative_base()

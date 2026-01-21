346d2396e951d2f6116d58ab56584d5eed7c80f6
Christian Muñoz <lalodm69@gmail.com>
2026-01-20 21:36:48 -0600

    Configurados CORS para permitir conexiones externas

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine
 import models
 from routers import users, boards, auth
 
 app = FastAPI(title="Trello Clone API")
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000", # Puerto común de React
+    "http://localhost:5173", # Puerto común de Vite/Vue
+    "https://tu-frontend.vercel.app", # Cuando subas tu frontend, agrégalo aquí
+    "*", # EL COMODÍN: Permite cualquier origen (útil para pruebas, pero úsalo con cuidado)
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Por ahora ponemos "*" para que no tengas problemas al probar
+    allow_credentials=True,
+    allow_methods=["*"], # Permite GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"], # Permite todos los headers (incluyendo el de Authorization para el Token)
+)
 
 # Esto evita que el Swagger se congele si la DB tarda en responder
 @app.on_event("startup")
